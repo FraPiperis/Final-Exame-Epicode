61e6b07ba07fea86d85fd1652ce690ba0e32f617
FraPiperis <FrancescaPiperis_00@outlook.it>
2025-05-12 18:42:42 +0200

    mod 8

--- Backend/Login.jsx
+++ Backend/Login.jsx
@@ -8,7 +8,8 @@ function Login() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:3000/login", {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/posts`)
+, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
