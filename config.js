0f37fb4c40ae7f7a8c0752d0706fb346bebf5f5f
FraPiperis <FrancescaPiperis_00@outlook.it>
2025-05-13 17:17:02 +0200

    final

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET
-  mongoUrl: process.env.MONGO_URL,
-  const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey';
-};
+// config.js
+export const MONGODB_URL = 'mongodb+srv://dzqwacsjg:618189748969395@cluster.mongodb.net/strive-blog?retryWrites=true&w=majority';
+export const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey';
+
